7196ae8e3d3941d487387d950aa409eca6f7b758
khanSaif@123 <khansaiflko0@gmail.com>
2026-08-15 18:15:08 +0530

    added all the chages

--- utils/newFeatures.js
+++ utils/newFeatures.js
@@ -1,11 +1,13 @@
+import { calculateWeight } from "../saif";
 
-
-let newFeature = "This is the new feature";
+let newFeatures = "This is the new feature";
 function newFeature(){
-    return newFeature;
+    return newFeatures;
 }
 
 let handleLogs = () => {
     console.log("Error is there in the new feature");
 }
 
+// here one new function required.
+calculateWeight(57);
